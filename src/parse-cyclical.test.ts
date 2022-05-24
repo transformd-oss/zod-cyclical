bb1a9891acfa2ba4e4ba5dda28787e1200764aee
Peter Boyer <8391902+ptboyer@users.noreply.github.com>
2022-05-24 16:34:39 +1000

    feat: working cyclical data structure walker

--- src/parse-cyclical.test.ts
+++ src/parse-cyclical.test.ts
@@ -4,36 +4,58 @@ import { parseCyclical } from "./parse-cyclical";
 it("should give me a correct validated node from id", () => {
   interface User {
     name: string;
-    friend?: User;
+    friend: User;
+    ofriend?: User;
   }
 
   const User: z.ZodType<User> = z.lazy(() =>
     z.object({
       name: z.string(),
-      friend: User.optional(),
+      friend: User,
+      ofriend: User.optional(),
     })
   );
 
   const userA: User = {
     name: "aaa",
+    friend: {} as User,
   };
 
   const userB: User = {
     name: "bbb",
+    friend: {} as User,
   };
 
   userA.friend = userB;
+  userA.ofriend = userB;
   userB.friend = userA;
+  userB.ofriend = userA;
 
-  const [user] = parseCyclical(userA, User);
-  expect(user).toMatchObject({
-    name: "aaa",
-    friend: {
-      name: "bbb",
+  const $ = parseCyclical(userA, User);
+  expect($).toMatchObject({
+    success: true,
+    data: {
+      name: "aaa",
       friend: {
-        name: "aaa",
-        friend: {},
+        name: "bbb",
+        friend: {
+          name: "aaa",
+          friend: {},
+        },
       },
     },
   });
+});
+
+it("test", () => {
+  {
+    const $ = z.object({ keyS: z.string() }).safeParse("abc");
+    expect($).toBeTruthy();
+  }
+  {
+    const $ = z
+      .object({ keyO: z.object({ keyO_: z.string() }) })
+      .safeParse({ keyO: { keyO_: true } });
+    expect($).toBeTruthy();
+  }
 });
